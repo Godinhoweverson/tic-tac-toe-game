bd6cd318974459393f4c5dd9c80937cabbb70956
Weverson Godinho <weverson.godinho@icloud.com>
2025-02-06 16:00:52 +0000

    Create and add style on initialGame component

---
+++ tic-tac-toe-game/src/components/InitialGame/InitialGame.jsx
@@ -0,0 +1,47 @@
+import logo from '../../assets/images/logo.svg';
+import iconx from '../../assets/images/icon-x-navy.svg';
+import iconXSilver from '../../assets/images/icon-x-silver.svg';
+import iconO from '../../assets/images/icon-o.svg';
+import iconOSilver from '../../assets/images/icon-o-silver.svg';
+import { useState } from 'react';
+
+export default function InitialGame(){
+
+const [pickMarkX, setPickMarkX] = useState('active-initialGame');
+const [pickMarkO, setPickMarkO] = useState('desactive-intialGame');
+
+function handleClickXmark(){
+    setPickMarkX('active-initialGame');
+    setPickMarkO('desactive-intialGame')
+}
+
+function handleClickOmark(){
+    setPickMarkO('active-initialGame')
+    setPickMarkX('desactive-intialGame');
+}
+    return(
+        <div className="box-initialGame">
+            <nav className="nav-initialGame">
+                <img src={logo} alt="Logo"/>
+            </nav>
+            <section className='box-pick-mark-initialGame'>
+                <p>PICK PLAYER 1’S MARK</p>
+                <div className='pick-mark-initialGame'>
+                    <button className={`btn-pick-initialGame-x ${pickMarkX}`}
+                    onClick={handleClickXmark}>
+                        <img src={pickMarkX === 'active-initialGame'? iconx : iconXSilver} alt="mark x"/>
+                    </button>
+                    <button className={`btn-pick-initialGame-o ${pickMarkO}`}
+                    onClick={handleClickOmark}>
+                        <img src={pickMarkO === 'active-initialGame'? iconO : iconOSilver} alt=""/>
+                    </button>
+                </div>
+                <p>REMEMBER : X GOES FIRST</p>
+            </section>
+            <div className="newGame-initalGame">
+                <button className='cpu'>NEW GAME (VS CPU)</button>
+                <button className='player'>NEW GAME  (VS PLAYER)</button>
+            </div>
+        </div>
+    )
+}
--- tic-tac-toe-game/vite.config.js
+++ tic-tac-toe-game/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      '5173-godinhoweve-tictactoega-l3stm32djmn.ws-eu117.gitpod.io', 
+    ],
+  },
 })
